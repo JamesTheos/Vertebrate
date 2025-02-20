31099395c976bbf8b993cd23e1d4cb8ad1bb371f
jub191 <93326054+jub191@users.noreply.github.com>
2025-02-20 13:05:06 +0100

    update header sidebar

---
+++ code/static/js/settingssidebar.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.settings');
+    const settingsidebar = document.querySelector('.settings-sidebar');
+    const sidebar = document.querySelector('.sidebar');
+
+    button.addEventListener('click', function() {
+        settingsidebar.classList.toggle('show-sidebar');
+        sidebar.classList.toggle('show-sidebar');
+    });
+});
